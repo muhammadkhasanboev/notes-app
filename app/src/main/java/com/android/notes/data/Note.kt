2d7f4c3c87568dac83f5bf9efb98affac74d703f
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-19 11:36:26 +0500

    Note: Entity class added

--- app/src/main/java/com/android/notes/data/Note.kt
+++ app/src/main/java/com/android/notes/data/Note.kt
@@ -1,6 +1,13 @@
 package com.android.notes.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName="notes_table")
 data class Note(
-    var id: Int
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val content: String,
+    val timeStamp: Long
 )
