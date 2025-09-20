8c12f76711ee942a277b779076b4a01d10a35056
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-20 11:40:56 +0500

    NotesDatabase: database class created

---
+++ app/src/main/java/com/android/notes/data/NoteDatabase.kt
@@ -0,0 +1,8 @@
+package com.android.notes.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class],version = 1, exportSchema = false)
+abstract class NoteDatabase: RoomDatabase() {
+}
