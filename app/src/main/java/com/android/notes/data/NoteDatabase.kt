c7e75dc5692033078e238fb54f2b96ed60f928cd
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-20 16:17:49 +0500

    database class added

--- app/src/main/java/com/android/notes/data/NoteDatabase.kt
+++ app/src/main/java/com/android/notes/data/NoteDatabase.kt
@@ -1,8 +1,24 @@
 package com.android.notes.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlin.jvm.java
 
 @Database(entities = [Note::class],version = 1, exportSchema = false)
 abstract class NoteDatabase: RoomDatabase() {
+    abstract fun NoteDao(): NoteDao
+
+    companion object{
+        @Volatile
+        private var Instance: NoteDatabase? = null
+        fun getDatabase(context: Context): NoteDatabase{
+            return Instance ?: synchronized(this){
+                Room.databaseBuilder(context, NoteDatabase::class.java, "item_database")
+                    .build()
+                    .also{Instance=it}
+            }
+        }
+    }
 }
