9755439bfca79a18f50c0a6c6c6cbcdf1cf29d17
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-21 17:33:04 +0500

    names and unused imports changed

--- app/src/main/java/com/android/notes/data/NoteDatabase.kt
+++ app/src/main/java/com/android/notes/data/NoteDatabase.kt
@@ -4,18 +4,17 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import kotlin.jvm.java
 
 @Database(entities = [Note::class],version = 1, exportSchema = false)
 abstract class NoteDatabase: RoomDatabase() {
-    abstract fun NoteDao(): NoteDao
+    abstract fun noteDao(): NoteDao
 
     companion object{
         @Volatile
         private var Instance: NoteDatabase? = null
         fun getDatabase(context: Context): NoteDatabase{
             return Instance ?: synchronized(this){
-                Room.databaseBuilder(context, NoteDatabase::class.java, "item_database")
+                Room.databaseBuilder(context, NoteDatabase::class.java, "note_database")
                     .build()
                     .also{Instance=it}
             }
