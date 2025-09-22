10e1cece863248f29de5198c15f5df82a905f97f
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-22 10:14:45 +0500

    Home viewmodel added

---
+++ app/src/main/java/com/android/notes/ui/theme/home/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.android.notes.ui.theme.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(){
+
+}
--- app/src/main/java/com/android/notes/ui/theme/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/android/notes/ui/theme/home/HomeScreenViewModel.kt
@@ -1,4 +1,33 @@
 package com.android.notes.ui.theme.home
 
-class HomeScreenViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.notes.data.Note
+import com.android.notes.data.OfflineNoteRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class HomeScreenViewModel(private val repository: OfflineNoteRepository): ViewModel() {
+
+    val notes: StateFlow<List<Note>> = repository.getAllNotes()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+
+    fun addNote(note: Note){
+        viewModelScope.launch {
+            repository.insertNote(note)
+        }
+    }
+
+    fun deleteNote(note: Note){
+        viewModelScope.launch {
+            repository.deleteNote(note)
+        }
+    }
 }
+
