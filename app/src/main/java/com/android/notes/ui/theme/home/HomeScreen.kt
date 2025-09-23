8c4e6de29bca25c86d02cc722fdae750900225d5
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-23 17:48:29 +0500

    top app bar added to home screen

--- app/src/main/java/com/android/notes/ui/theme/home/HomeScreen.kt
+++ app/src/main/java/com/android/notes/ui/theme/home/HomeScreen.kt
@@ -1,11 +1,21 @@
 package com.android.notes.ui.theme.home
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.android.notes.NotesTopBar
 import com.android.notes.navigation.NavigationDestination
 import com.android.notes.R
 
@@ -24,7 +34,25 @@ fun HomeScreen(
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-
+            NotesTopBar(
+                title = stringResource(id = R.string.home),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {}, //adding new note screen should be added
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.item_entry_title)
+                )
+            }
         }
-    ) {  }
+    ) {
+        innerPadding ->
+    }
 }
