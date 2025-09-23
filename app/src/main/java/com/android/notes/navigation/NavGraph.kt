c5c9739da439286496229bda1e036ecf5be497e5
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-23 15:05:48 +0500

    NavGraph added

---
+++ app/src/main/java/com/android/notes/navigation/NavGraph.kt
@@ -0,0 +1,13 @@
+package com.android.notes.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun NotesNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+
+}
