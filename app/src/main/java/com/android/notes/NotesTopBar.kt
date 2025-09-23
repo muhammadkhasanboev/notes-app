e8024bd59c662e66032f9b3e21439ffa086c7c0c
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-23 17:38:46 +0500

    Global top app bar added

--- app/src/main/java/com/android/notes/NotesTopBar.kt
+++ app/src/main/java/com/android/notes/NotesTopBar.kt
@@ -1,6 +1,17 @@
 package com.android.notes
 
+import com.android.notes.R
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.android.notes.navigation.NotesNavHost
 import androidx.navigation.NavHostController
@@ -11,7 +22,29 @@ fun NotesApp(navController: NavHostController = rememberNavController()){
     NotesNavHost(navController = navController)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NotesTopBar(){
+fun NotesTopBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? =null,
+    navigateUp: () -> Unit = {}
+){
+    CenterAlignedTopAppBar(
+        title = { Text(title)},
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Filled.ArrowBack,
+                        contentDescription = stringResource(id=R.string.back_button)
+                    )
+                }
+            }
+        }
 
+    )
 }
