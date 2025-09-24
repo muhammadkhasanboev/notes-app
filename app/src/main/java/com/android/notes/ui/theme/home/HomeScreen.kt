6807a1b7631922fb4c91b598cae90099f2afa909
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-24 11:50:26 +0500

    HomeScreen: HomeBody composable created

--- app/src/main/java/com/android/notes/ui/theme/home/HomeScreen.kt
+++ app/src/main/java/com/android/notes/ui/theme/home/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.android.notes.ui.theme.home
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -9,15 +12,20 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.notes.NotesTopBar
 import com.android.notes.navigation.NavigationDestination
 import com.android.notes.R
+import com.android.notes.data.Note
 
 object HomeDestination : NavigationDestination {
     override val route = "home"
@@ -54,5 +62,39 @@ fun HomeScreen(
         }
     ) {
         innerPadding ->
+        HomeBody(
+           noteList = listOf(),
+            onNoteClick = { },
+            modifier = modifier.fillMaxSize(),
+            contentPadding = innerPadding
+        )
     }
 }
+
+@Composable
+private fun HomeBody(
+    noteList: List<Note>,
+    onNoteClick: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ){
+        if(noteList.isNotEmpty()){
+            Text(
+                text = stringResource(id = R.string.no_note_description),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = modifier.padding(contentPadding)
+            )
+        }
+    }
+}
+
+//@Composable
+//@Preview(showSystemUi = true)
+//fun check(){
+//    HomeScreen()
+//}
